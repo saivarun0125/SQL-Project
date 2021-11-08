461e957bd909ec50abdf8515dd7faa4d87eff818
ccsherri <ccsherri@ncsu.edu>
2021-11-07 21:20:48 -0500

    added RewardsCheck

--- wolfcity/src/models/RewardsCheck.java
+++ wolfcity/src/models/RewardsCheck.java
@@ -1,4 +1,56 @@
 package models;
 
 public class RewardsCheck {
+	
+//	RewardsCheck(checkID, amount, staffID, memberID)
+//	checkID is the primary key
+//	amount, staffID, and memberID are not allowed to be null
+	
+	private int checkID;
+	private int amount;
+	private int staffID;
+	private int memberID;
+	
+	public RewardsCheck(int checkID, int amount, int staffID, int memberID) {
+		this.checkID = checkID;
+		this.amount = amount;
+		this.staffID = staffID;
+		this.memberID = memberID;
+		
+	}
+
+	public int getCheckID() {
+		return checkID;
+	}
+
+	public void setCheckID(int checkID) {
+		this.checkID = checkID;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getStaffID() {
+		return staffID;
+	}
+
+	public void setStaffID(int staffID) {
+		this.staffID = staffID;
+	}
+
+	public int getMemberID() {
+		return memberID;
+	}
+
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
+	
+	
+ 
 }
